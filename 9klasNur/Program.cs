d3124d867940b50c2d33c50a7fd0cf22e959fd28
Айшенур <ayshenurhasanova@gmail.com>
2025-06-12 10:56:18 +0300

    Add files via upload

---
+++ 9klasNur/Program.cs
@@ -0,0 +1,101 @@
+﻿using System.Collections.Immutable;
+using System.Globalization;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace _9klasNur
+{
+    internal class BankAccound
+    {
+        private int id;
+        private double balance;
+
+        public int ID
+        {
+            get { return id; }
+            set { id = value; }
+        }
+        public double Balance
+        {
+            get { return balance; }
+        }
+        public void Deposit(double amount)
+        {
+            if (amount > 0)
+            {
+                balance += amount;
+            }
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (amount < 0 && amount <= balance)
+            {
+                balance -= amount;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Account ID : {id} Balance: {balance:F2}"; 
+                
+        }
+
+
+        public static void Main()
+        {
+
+            BankAccount acc = new BankAccound();
+            acc.ID = 1;
+            acc.Deposit(15);
+            acc.Withdraw(5);
+            Console.WriteLine(acc.ToString());
+
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
